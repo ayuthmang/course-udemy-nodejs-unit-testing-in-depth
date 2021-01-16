52260be7cf248751a8df08cdf9cb4a32e3499f94
Ayuth Mangmesap <ayuth.mangmesap@scb.co.th>
2021-01-16 22:00:10 +0700

    Section 3: Basic of Testing - Chai Addons & More Promises

--- full-app-with-tests/lib/demo.test.js
+++ full-app-with-tests/lib/demo.test.js
@@ -1,5 +1,7 @@
 const chai = require('chai')
 const expect = chai.expect
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
 
 var demo = require('./demo')
 
@@ -39,6 +41,16 @@ describe('demo', () => {
       return demo.addPromise(1, 2).then((result) => {
         expect(result).to.equal(3)
       })
+    })
+
+    it('should test promise with async await', async () => {
+      let result = await demo.addPromise(1, 2)
+
+      expect(result).to.equal(3)
+    })
+
+    it('should test promise with chai promises', async () => {
+      await expect(demo.addPromise(1, 2)).to.eventually.equal(3)
     })
   })
 })
